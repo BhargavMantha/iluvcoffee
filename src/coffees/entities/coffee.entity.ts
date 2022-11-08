ded8890b4b239e9c3b9588d0b483c996584fcfca
Bhargav Mantha <manthabhargav@gmail.com>
2022-11-08 08:08:08 +0530

    a new Entity flavor and update in the existing Coffee entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,28 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity()
 export class Coffee {
-  id: number;
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column()
   name: string;
+  @Column({ nullable: true })
+  description: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @Column({ default: 0 })
+  recommendations: number;
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffee, { cascade: true })
+  flavors: Flavor[];
 }
