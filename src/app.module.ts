c63d908dc6a811f95124f36a27058bdc4899bcfa
Bhargav Mantha <manthabhargav@gmail.com>
2022-11-13 12:36:34 +0530

    changed the variable names

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import * as Joi from '@hapi/joi';
       host: process.env.DATABASE_HOST, // database host
       port: +process.env.DATABASE_PORT, // database host
       username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_password, // user password
+      password: process.env.DATABASE_PASSWORD, // user password
       database: process.env.DATABASE_NAME, // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: true // your entities will be synced with the database(recommended: disable in prod)
